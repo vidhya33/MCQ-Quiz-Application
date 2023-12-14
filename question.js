39cf8981e0e7fea7d4ba4c38495fe4d9ca282e85
vidhya33 <133134452+vidhya33@users.noreply.github.com>
2023-12-14 12:34:26 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Mechanism by which a single piece of code can manipulate many different data types without explicitly having a separate entity for each data type is known as ?",
+    answer: "Generics",
+    options: [
+      "Generics",
+      "Encapsulation",
+      "Abstraction",
+      "Polymorphism"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In Javascript each frame in a frame set is represented as a?",
+    answer: "frame object",
+    options: [
+      "object",
+      "frame",
+      "frame object",
+      "frame set"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The purpose of a DTD is to define legal building blocks of which document?",
+    answer: "XML",
+    options: [
+      "HTML",
+      "XML",
+      "CSS",
+      "JS"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not a method to read a parameters from a form?",
+    answer: "getParameter(String pname)",
+    options: [
+      "getParameterValues(String pname)",
+      "getParameterNames()",
+      "getParameter(String pname)",
+      "getParameterValues()"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
